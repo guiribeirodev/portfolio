709be9591d8311bd5e453722d4bb85968d5563c5
Guilherme Ribeiro <guiribeirodev@gmail.com>
2024-05-29 14:40:39 +0000

    Update template with my personal informations

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Guilherme Ribeiro de Carvalho",
+  title: "Web Developer",
+  email: "guiribeirodev@gmail.com",
+  gitHub: "guiribeirodev",
+  instagram: "nonno",
+  linkedIn: "guiribeirodev",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "mnono",
+  youTube: "nono",
 };
 
 const primaryColor = "#4E567E";
